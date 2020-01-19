193c27042adb70e74ea900032a53444fb66bd9f5
MrGuizao <guigen131@gmail.com>
2020-01-19 15:42:23 -0300

    frontend quase pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,100 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+     return (
+          <div id="app">
+               <aside>
+                    <form action="">
+                         <div className="input-block">
+                              <label htmlFor="github_username">
+                                   Usuário do Github
+                              </label>
+                              <input
+                                   name="github_username"
+                                   id="github_username"
+                                   required
+                              />
+                         </div>
+
+                         <div className="input-block">
+                              <label htmlFor="techs">Tecnologias</label>
+                              <input name="techs" id="techs" required />
+                         </div>
+
+                         <div className="input-group">
+                              <div className="input-block">
+                                   <label htmlFor="">Latitude</label>
+                                   <input
+                                        name="latitude"
+                                        id="latitude"
+                                        required
+                                   />
+                              </div>
+                              <div className="input-block">
+                                   <label htmlFor="">Longitude</label>
+                                   <input
+                                        name="longitude"
+                                        id="longitude"
+                                        required
+                                   />
+                              </div>
+                         </div>
+                         <button type="submit">Salvar</button>
+                    </form>
+               </aside>
+               {/* MAIN */}
+               <main>
+                    <ul>
+                         <li className="dev-item">
+                              <header>
+                                   <img src="https://unsplash.it/100/100" alt="" />
+                                   <div className="user-info">
+                                     <strong>Nome</strong>
+                                     <span>techs</span>
+                                   </div>
+                              </header>
+                              <p>resumo</p>
+                              <a href="https://github.com/nome">Acessar perfil</a>
+                         </li>
+                         <li className="dev-item">
+                              <header>
+                                   <img src="https://unsplash.it/100/100" alt="" />
+                                   <div className="user-info">
+                                     <strong>Nome</strong>
+                                     <span>techs</span>
+                                   </div>
+                              </header>
+                              <p>resumo</p>
+                              <a href="https://github.com/nome">Acessar perfil</a>
+                         </li>
+                         <li className="dev-item">
+                              <header>
+                                   <img src="https://unsplash.it/100/100" alt="" />
+                                   <div className="user-info">
+                                     <strong>Nome</strong>
+                                     <span>techs</span>
+                                   </div>
+                              </header>
+                              <p>resumo</p>
+                              <a href="https://github.com/nome">Acessar perfil</a>
+                         </li>
+                         <li className="dev-item">
+                              <header>
+                                   <img src="https://unsplash.it/100/100" alt="" />
+                                   <div className="user-info">
+                                     <strong>Nome</strong>
+                                     <span>techs</span>
+                                   </div>
+                              </header>
+                              <p>resumo</p>
+                              <a href="https://github.com/nome">Acessar perfil</a>
+                         </li>
+
+                    </ul>
+               </main>
+          </div>
+     );
 }
 
 export default App;
